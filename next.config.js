7fa77e818c00377431a248e8819c6bcf6bf7438f
Swasthik K Shetty <swasthik.mk3@gmail.com>
2026-07-19 18:51:01 +0530

    Configure GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // Export as a static website
+  output: "export",
+
+  // Required for GitHub Pages
+  basePath: "/Portfolio",
+  assetPrefix: "/Portfolio/",
+
+  // Next.js Image component compatibility
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
